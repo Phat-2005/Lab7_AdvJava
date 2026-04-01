e25fa34857f2ddc877622e5e60079f2c2a447178
Phat-2005 <buigiaphat2005@gmail.com>
2026-04-01 12:43:52 +0700

    Add files via upload

---
+++ DemoUser/src/main/java/com/howto/DemoUser/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.howto.DemoUser.controller;
+
+import com.howto.DemoUser.model.User;
+import com.howto.DemoUser.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+    
+    @GetMapping
+    public List<User> findAllUsers() {
+        return userRepository.findAll();
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findUserById(@PathVariable(value = "id") long id) {
+        Optional<User> user = userRepository.findById(id);
+     
+        if(user.isPresent()) {
+            return ResponseEntity.ok().body(user.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public User saveUser(@Validated @RequestBody User user) {
+        return userRepository.save(user);
+    }
+    
+   
+}
---
+++ DemoUser/src/main/java/com/howto/DemoUser/model/User.java
@@ -0,0 +1,44 @@
+package com.howto.DemoUser.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String name;
+
+    // Constructor mặc định bắt buộc cho JPA
+    public User() {
+    }
+
+    // Constructor tiện dụng
+    public User(String name) {
+        this.name = name;
+    }
+
+    // Getter và Setter
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
