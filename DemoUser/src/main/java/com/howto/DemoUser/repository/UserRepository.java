b4811b1826702a1fcb470a576576d9021a3af424
Phat-2005 <buigiaphat2005@gmail.com>
2026-04-01 12:38:34 +0700

    Delete DemoUser directory

--- DemoUser/src/main/java/com/howto/DemoUser/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.howto.DemoUser.repository;
-
-import com.howto.DemoUser.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
